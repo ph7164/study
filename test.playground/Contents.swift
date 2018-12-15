a7d97470a005a300a3714ca5b878e5da179b9e37
ph7164 <ph71640@gmail.com>
2018-12-15 19:04:16 +0900

    first commit

---
+++ test.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var value1: Int = 10
+var message: String="안녕"
+
+var value2:String=String(value1)
+print(message+value2)
+
+var value3:String="20"
+var value4:Int?=Int(value3)
+print(value4!)

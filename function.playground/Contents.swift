9f2e7af43cabecc060cee030a066e7cf7496356b
ph7164 <ph71640@gmail.com>
2018-12-18 20:16:39 +0900

    third

--- function.playground/Contents.swift
+++ function.playground/Contents.swift
@@ -4,6 +4,7 @@ func show(){
 
 show()
 
+
 // quiz 07
 func showName(){
     let name : String = "philhwa"
@@ -11,6 +12,7 @@ func showName(){
 }
 
 showName()
+
 
 func show2(){
     var count=0
@@ -33,6 +35,7 @@ func show2(){
 show2()
 
 
+
 func sum(start:Int, end:Int) -> Int{
     var count=0
     for i in start ..< end {
@@ -47,3 +50,34 @@ print("sum(1,11)->\(result)")
 
 result = sum(start:1,end:101)
 print("sum(1,101)->\(result)")
+
+
+func add(a:Int,b:Int){
+    print("add 호출됨 : \(a), \(b)")
+}
+
+add(a:10,b:20)
+
+
+func add2(first a:Int, second b:Int){
+    print("add2 호출됨 : \(a), \(b)")
+}
+
+add2(first:10, second:20)
+
+
+func add3(_ a:Int, _ b:Int){
+    print("add3 호출됨 : \(a), \(b)")
+}
+
+add3(10,20)
+
+
+// quiz08
+
+func multiply(_ a:Int, _ b:Int) -> Int{
+    return a*b
+}
+
+let res = multiply(10,10)
+print("result : \(res)")
